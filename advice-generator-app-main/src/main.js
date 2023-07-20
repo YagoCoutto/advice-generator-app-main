04752e43402330796b32144fcb6fc29516da7c3f
Yago Coutto <62577739+YagoCoutto@users.noreply.github.com>
2023-07-19 22:12:32 -0300

    update

--- advice-generator-app-main/src/main.js
+++ advice-generator-app-main/src/main.js
@@ -1,7 +1,16 @@
-const advice = document.querySelector('#content');
+const advice = document.querySelector('#cite');
+const strong = document.querySelector('strong');
+const btt = document.querySelector('button');
 
-fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => {
-        advice.innerHTML = data.slip.advice;
-    });
+function updateApi() {
+
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => {
+            advice.innerHTML = `&#8220;${data.slip.advice}&#8221;`;
+            strong.innerHTML = ` #${data.slip.id}`
+        });
+}
+updateApi()
+
+btt.addEventListener('click', updateApi)
